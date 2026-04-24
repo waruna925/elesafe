a8becb66e85be974e23446f11af69c9207cb747e
waruna925 <warunasuraweera925@gmail.com>
2026-04-24 19:08:05 +0530

    Initial commit

---
+++ src/main/java/com/example/jkr/elesafe/ElesafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jkr.elesafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElesafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElesafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jkr/elesafe/ElesafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jkr.elesafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElesafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
