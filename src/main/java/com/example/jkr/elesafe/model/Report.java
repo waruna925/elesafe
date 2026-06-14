127f5ba7431fdda01eb2816a534356f6b2ff4a00
waruna925 <warunasuraweera925@gmail.com>
2026-06-14 12:34:38 +0530

    fix timezone gap

--- src/main/java/com/example/jkr/elesafe/model/Report.java
+++ src/main/java/com/example/jkr/elesafe/model/Report.java
@@ -1,5 +1,6 @@
 package com.example.jkr.elesafe.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,9 +22,10 @@ public abstract class Report {
     private String reporterId;
     private String district;
     private String village;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
     private LocalDateTime dateTime;
 
-    // ✅ ADD THESE TWO FIELDS
     private Double latitude;
     private Double longitude;
 }
